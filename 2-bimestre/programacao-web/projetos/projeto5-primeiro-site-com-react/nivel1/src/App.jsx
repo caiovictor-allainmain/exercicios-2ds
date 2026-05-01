f1c7bca65e90c1a081c6f67203f53a1bcef7e265
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2026-05-01 20:46:46 +0000

    concluído

--- 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel1/src/App.jsx
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel1/src/App.jsx
@@ -4,7 +4,11 @@ import { Footer } from './components/Footer'
 export default function App() {
   return (
     <div>
-      {/* Adicione seus componentes abaixo dessa linha */}
+      
+      <Header />
+
+      <Footer />
+      
     </div>
   )
 }
---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel1/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+    return(
+        <div>
+            <p>2026 Meu Site React</p>
+        </div>
+    )
+}
