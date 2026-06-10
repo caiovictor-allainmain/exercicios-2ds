56a89ebc2668d6f3d89f24fbe684f7eee2c0eb10
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2026-06-10 00:19:47 +0000

    feitoooo

---
+++ 2-bimestre/programacao-web/aula04-criando-primeiros-componentes/React/src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo from './components/titulo'
+import Paragrafo from './components/paragrafo'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <Titulo />
+      <Paragrafo />
+    </div>
+  )
+}
+
+export default App
